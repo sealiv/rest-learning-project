040cc95d95761defaa1156f128b4ca072f0f26b6
Sealiv <sai.79.ua@gmail.com>
2022-06-05 18:51:20 +0300

    Created Event.

---
+++ event-service-dto/src/main/java/org/aleks4ay/dto/Event.java
@@ -0,0 +1,60 @@
+package org.aleks4ay.dto;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private long id;
+    private String title;
+    private String place;
+    private String speaker;
+    private String eventType;
+    private LocalDateTime dateTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
