9a9a676fbfcea0bf92803c9e659d3a4043adb5dc
Sealiv <sai.79.ua@gmail.com>
2022-06-09 18:11:40 +0300

    Added Swagger 2.0

--- event-service-rest/src/main/java/org/aleks4ay/rest/EventServiceController.java
+++ event-service-rest/src/main/java/org/aleks4ay/rest/EventServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -34,8 +35,8 @@ public class EventServiceController {
         return eventService.getAllEvents();
     }
 
-    @GetMapping(path = "/search/{title}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Event> getAllEventsByTitle(@PathVariable(value = "title") String title) {
+    @GetMapping(path = "/getByTitle", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Event> getAllEventsByTitle(@PathParam(value = "title") String title) {
         return eventService.getAllEventsByTitle(title);
     }
 
